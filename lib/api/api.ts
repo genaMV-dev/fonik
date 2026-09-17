81ad92156d46b750fcb505dc8a9913cdc3d4925e
Ign1s123 <genanocheats@gmail.com>
2026-09-18 01:07:41 +0300

    feat: add registration page and user authentication
    - Implemented a new registration page with form validation using Formik and Yup.
    - Added user registration API integration with Axios.
    - Created a Zustand store for managing authentication state.
    - Updated Header component to conditionally render navigation links based on authentication status.
    - Added error handling for registration errors.
    - Introduced new CSS styles for the registration form and updated Header styles.
    - Added necessary dependencies: axios, yup, and zustand.

---
+++ lib/api/api.ts
@@ -0,0 +1,40 @@
+import axios from "axios"
+import { AuthUser } from "../store/authStore";
+
+export interface RegisterDTO {
+  username: string 
+  email: string
+  password: string
+}
+
+export interface User {
+  _id: string
+  name: string
+  email: string
+  avatar?: string
+  createdAt?: string
+  updatedAt?: string
+}
+
+export interface RegisterResponse {
+  user: AuthUser
+  status: number
+  message: string
+  data: {
+    user: User
+    accessToken?: string
+  }
+}
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+
+  withCredentials: true,
+})
+
+export const registerUser = async (
+  userData: RegisterDTO,
+): Promise<RegisterResponse> => {
+  const { data } = await api.post<RegisterResponse>("/auth/register", userData)
+  return data
+}
